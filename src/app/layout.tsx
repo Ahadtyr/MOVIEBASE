8e6f63d418b99e1d54917511971ab7799ce49a3f
Ahad Domain <ahaddomain560@gmail.com>
2025-10-19 08:36:17 +0000

    use this img as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />

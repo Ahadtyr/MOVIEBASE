82d9bd282ce43e49158c037d12f2bc316b6e2db8
Ahad Domain <ahaddomain560@gmail.com>
2025-10-19 09:49:37 +0000

    Try fixing this error: `Console Error: Error fetching from TMDb: 401 Una

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -1,7 +1,6 @@
 
 import type { Movie, TVShow, TMDBCastMember, Genre, TVSeason, TVSeasonDetails } from './types';
 
-const ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
 const BASE_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_BASE_URL_W500 = 'https://image.tmdb.org/t/p/w500';
 export const IMAGE_BASE_URL_ORIGINAL = 'https://image.tmdb.org/t/p/original';

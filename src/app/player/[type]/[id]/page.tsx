5cc0626b21f8da69eab12d7a8f974104c751aa61
Ahad Domain <ahaddomain560@gmail.com>
2025-11-01 09:28:25 +0000

    but we needed download option player

--- src/app/player/[type]/[id]/page.tsx
+++ src/app/player/[type]/[id]/page.tsx
@@ -46,14 +46,14 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     title = movie.title;
     itemUrl = `/movie/${tmdbId}`;
     embedSrc1 = `https://vidsrc.to/embed/movie/${tmdbId}`;
-    embedSrc2 = `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1`;
+    embedSrc2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}`;
   } else {
     const show = await getTVShowDetails(tmdbId);
     if (!show) notFound();
     title = `${show.name} - S${seasonNumber} E${episodeNumber}`;
     itemUrl = `/tv-show/${tmdbId}`;
     embedSrc1 = `https://vidsrc.to/embed/tv/${tmdbId}/${seasonNumber}/${episodeNumber}`;
-    embedSrc2 = `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1&s=${seasonNumber}&e=${episodeNumber}`;
+    embedSrc2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}&season=${seasonNumber}&episode=${episodeNumber}`;
 
     // Fetch all seasons data
     if (show.seasons) {

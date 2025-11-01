3d417cf83784fdc6676a079cf6c0ffa8e9fd4d6b
Ahad Domain <ahaddomain560@gmail.com>
2025-11-01 09:26:10 +0000

    Did Not Connect: Potential Security Issue
    Zen detected a potential secu

--- src/app/player/[type]/[id]/page.tsx
+++ src/app/player/[type]/[id]/page.tsx
@@ -46,14 +46,14 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     title = movie.title;
     itemUrl = `/movie/${tmdbId}`;
     embedSrc1 = `https://vidsrc.to/embed/movie/${tmdbId}`;
-    embedSrc2 = `https://vidsrc.pro/embed/movie/${tmdbId}`;
+    embedSrc2 = `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1`;
   } else {
     const show = await getTVShowDetails(tmdbId);
     if (!show) notFound();
     title = `${show.name} - S${seasonNumber} E${episodeNumber}`;
     itemUrl = `/tv-show/${tmdbId}`;
     embedSrc1 = `https://vidsrc.to/embed/tv/${tmdbId}/${seasonNumber}/${episodeNumber}`;
-    embedSrc2 = `https://vidsrc.pro/embed/tv/${tmdbId}/${seasonNumber}/${episodeNumber}`;
+    embedSrc2 = `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1&s=${seasonNumber}&e=${episodeNumber}`;
 
     // Fetch all seasons data
     if (show.seasons) {

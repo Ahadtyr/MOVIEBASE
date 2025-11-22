2b67ad3371b8bc37eb41698230b1adab7d5a141c
Ahad Domain <ahaddomain560@gmail.com>
2025-11-22 09:59:24 +0000

    Hmm. We’re having trouble finding that site.
    We can’t connect to the se

--- src/app/player/[type]/[id]/page.tsx
+++ src/app/player/[type]/[id]/page.tsx
@@ -49,7 +49,7 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     itemUrl = `/movie/${tmdbId}`;
     embedSrc1 = `https://watchsb.com/e/${tmdbId}`;
     embedSrc2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}`;
-    embedSrc3 = `https://vidfast.us/embed/${tmdbId}`;
+    embedSrc3 = `https://vidsrc.to/embed/movie/${tmdbId}`;
   } else {
     const show = await getTVShowDetails(tmdbId);
     if (!show) notFound();
@@ -57,7 +57,7 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     itemUrl = `/tv-show/${tmdbId}`;
     embedSrc1 = `https://watchsb.com/e/${tmdbId}-${seasonNumber}-${episodeNumber}`;
     embedSrc2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}&season=${seasonNumber}&episode=${episodeNumber}`;
-    embedSrc3 = `https://vidfast.us/embed/${tmdbId}?s=${seasonNumber}&e=${episodeNumber}`;
+    embedSrc3 = `https://vidsrc.to/embed/tv/${tmdbId}/${seasonNumber}/${episodeNumber}`;
 
     // Fetch all seasons data
     if (show.seasons) {

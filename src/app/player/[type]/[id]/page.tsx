5f4a1564e97e60b4d845686d1f9ef6b5909037e7
Ahad Domain <ahaddomain560@gmail.com>
2025-11-22 14:25:56 +0000

    ansi.to in 4 player

--- src/app/player/[type]/[id]/page.tsx
+++ src/app/player/[type]/[id]/page.tsx
@@ -58,7 +58,7 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     embedSrc.p1 = `https://watchsb.com/e/${tmdbId}`;
     embedSrc.p2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}`;
     embedSrc.p3 = `https://player.vidplus.to/embed/movie/${tmdbId}`;
-    embedSrc.p4 = `https://watchsb.com/e/${tmdbId}`; // Reverted to watchsb
+    embedSrc.p4 = `https://ansi.to/e/${tmdbId}`;
   } else {
     const show = await getTVShowDetails(tmdbId);
     if (!show) notFound();
@@ -67,7 +67,7 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     embedSrc.p1 = `https://watchsb.com/e/${tmdbId}-${seasonNumber}-${episodeNumber}`;
     embedSrc.p2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}&season=${seasonNumber}&episode=${episodeNumber}`;
     embedSrc.p3 = `https://player.vidplus.to/embed/tv/${tmdbId}?s=${seasonNumber}&e=${episodeNumber}`;
-    embedSrc.p4 = `https://watchsb.com/e/${tmdbId}-${seasonNumber}-${episodeNumber}`; // Reverted to watchsb
+    embedSrc.p4 = `https://ansi.to/e/${tmdbId}?s=${seasonNumber}&e=${episodeNumber}`;
 
     if (show.seasons) {
         const seasonDetailPromises = show.seasons

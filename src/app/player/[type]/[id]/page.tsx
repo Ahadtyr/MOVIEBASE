210f189a023fbfe9fa9886ce62e4bbfb374c07c0
Ahad Domain <ahaddomain560@gmail.com>
2025-11-22 10:49:30 +0000

    <video class="MuiBox-root mui-17jk45o" crossorigin="anonymous" preload="

--- src/app/player/[type]/[id]/page.tsx
+++ src/app/player/[type]/[id]/page.tsx
@@ -49,7 +49,7 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     itemUrl = `/movie/${tmdbId}`;
     embedSrc1 = `https://watchsb.com/e/${tmdbId}`;
     embedSrc2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}`;
-    embedSrc3 = `https://player.videasy.net/movie/${tmdbId}`;
+    embedSrc3 = `https://vidfast.pro/embed/movie/${tmdbId}`;
   } else {
     const show = await getTVShowDetails(tmdbId);
     if (!show) notFound();
@@ -57,7 +57,7 @@ export default async function PlayerPage({ params, searchParams }: PlayerPagePro
     itemUrl = `/tv-show/${tmdbId}`;
     embedSrc1 = `https://watchsb.com/e/${tmdbId}-${seasonNumber}-${episodeNumber}`;
     embedSrc2 = `https://embed.smashystream.com/playere.php?tmdb=${tmdbId}&season=${seasonNumber}&episode=${episodeNumber}`;
-    embedSrc3 = `https://player.videasy.net/tv/${tmdbId}/${seasonNumber}/${episodeNumber}`;
+    embedSrc3 = `https://vidfast.pro/embed/tv/${tmdbId}?s=${seasonNumber}&e=${episodeNumber}`;
 
     // Fetch all seasons data
     if (show.seasons) {
